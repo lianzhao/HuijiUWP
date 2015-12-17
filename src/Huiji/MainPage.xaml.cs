33a1355b2e7291471398bfbcf4dc32d559d2219a
lianzhao <knife.fe@gmail.com>
2015-12-17 17:15:02 +0800

    demo

--- src/Huiji/MainPage.xaml.cs
+++ src/Huiji/MainPage.xaml.cs
@@ -3,9 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Data.Json;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -45,6 +48,28 @@ namespace Huiji
                     DependencyProperty.Register("StrongTypeViewModel", typeof(MainPage_Model), typeof(MainPage), new PropertyMetadata(null));
 
 
-
+        private async void MainPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var http = new HttpClient())
+                {
+                    var resp = await
+                        http.GetAsync(
+                            "http://coppermind.huiji.wiki/api.php?action=parse&page=%E6%B2%99%E5%85%B0%C2%B7%E8%BE%BE%E7%93%A6&prop=text&format=json").ConfigureAwait(false);
+                    var json = await resp.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    var html = JsonValue.Parse(json).GetObject().GetNamedObject("parse").GetNamedObject("text").GetNamedString("*");
+                    await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {
+                        webView.NavigateToString(html);
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                throw;
+            }
+        }
     }
 }
